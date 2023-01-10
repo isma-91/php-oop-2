e614e029e2dd41cf67add5de9909a9c3824224fd
isma-91 <isma91@hotmail.it>
2023-01-10 14:24:31 +0100

    First commit

---
+++ Products.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Products {
+
+public $name;
+public $description;
+protected $price;
+
+public function setPrice($price) 
+{
+  if (is_integer($price) && $price > 0) {
+			$this->price = $price;
+		}
+}
+
+public function getPrice()
+{
+return $this->price;
+}
+}
